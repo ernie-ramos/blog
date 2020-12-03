48e796af8ad2e341ff449f96f98c371fe9ee08b3
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-12-03 13:35:57 -0500

    refactored with .chain()

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,11 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts()); //redux thunk will pick this up and update reducer
 
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  userIds.forEach((id) => dispatch(fetchUser(id)));
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async (dispatch) => {
